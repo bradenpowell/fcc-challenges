c3b33d85f64be63f089b45b1f21138d99fe7d490
Braden Powell <bradenpowell2@gmail.com>
2018-06-09 02:36:13 -0600

    background color for clear weather

--- frontend/local-weather/weather.js
+++ frontend/local-weather/weather.js
@@ -21,7 +21,7 @@ function getWeather(lat, lon) {
     var requestURL = apiLink + lat + "&" + lon;
     $.ajax({url: requestURL, success: function(result) {
         var tempInF = Math.round((result.main.temp) * 1.8 + 32);
-        $("#temp").text(tempInF + String.fromCharCode(176) + "F");
+        $("#temp").text(tempInF + String.fromCharCode(176) + "");
         $("#desc").text(result.weather[0].description); //FIXME: make this into uppercase?
         $("#city").text(result.name);
         getIcon(result.weather[0].main);
@@ -31,20 +31,23 @@ function getWeather(lat, lon) {
 
 function getIcon(status) {
     var desc = status.toLowerCase();
-        switch (desc) {
-            case 'thunderstorm':
-                $('.wi').addClass('wi-storm-showers');
-            case 'drizzle':
-                $('.wi').addClass('wi-sprinkle');
-            case 'rain':
-                $('.wi').addClass('wi-rain');
-            case 'snow':
-                $('.wi').addClass('wi-snow');
-            case 'atmosphere':
-                $('.wi').addClass('wi-fog');
-            case 'clear':
-                $('.wi').addClass('wi-day-sunny');
-            case 'clouds':
-                $('.wi').addClass('wi-cloudy');
-        }
+    $('body').addClass(status)
+    
+    switch (desc) {
+        case 'thunderstorm':
+            $('.wi').addClass('wi-storm-showers');
+        case 'drizzle':
+            $('.wi').addClass('wi-sprinkle');
+        case 'rain':
+            $('.wi').addClass('wi-rain');
+        case 'snow':
+            $('.wi').addClass('wi-snow');
+        case 'atmosphere':
+            $('.wi').addClass('wi-fog');
+        case 'clear':
+            $('.wi').addClass('wi-day-sunny');
+            
+        case 'clouds':
+            $('.wi').addClass('wi-cloudy');
+    }
 }
