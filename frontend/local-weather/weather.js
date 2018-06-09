d3a79d74bcfd836384b6b0504c36b2ef9b10f622
Braden Powell <bradenpowell2@gmail.com>
2018-06-09 01:29:28 -0600

    icon changes based on "main" category

--- frontend/local-weather/weather.js
+++ frontend/local-weather/weather.js
@@ -26,5 +26,27 @@ function getWeather(lat, lon) {
         $("#temp").text(tempInF + String.fromCharCode(176) + "F");
         $("#desc").text(result.weather[0].description); //FIXME: make this into uppercase?
         $("#city").text(result.name);
+        getIcon(result.weather[0].main);
+
     }})
+}
+
+function getIcon(status) {
+    var desc = status.toLowerCase();
+        switch (desc) {
+            case 'thunderstorm':
+                $('.wi').addClass('wi-storm-showers');
+            case 'drizzle':
+                $('.wi').addClass('wi-sprinkle');
+            case 'rain':
+                $('.wi').addClass('wi-rain');
+            case 'snow':
+                $('.wi').addClass('wi-snow');
+            case 'atmosphere':
+                $('.wi').addClass('wi-fog');
+            case 'clear':
+                $('.wi').addClass('wi-day-sunny');
+            case 'clouds':
+                $('.wi').addClass('wi-cloudy');
+        }
 }
