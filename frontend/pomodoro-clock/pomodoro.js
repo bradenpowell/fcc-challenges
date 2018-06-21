36cc4933b6493b634444e56b8c8f2bd376a1d1c9
Braden Powell <bradenpowell2@gmail.com>
2018-06-21 15:35:14 -0600

    ghost start button

--- frontend/pomodoro-clock/pomodoro.js
+++ frontend/pomodoro-clock/pomodoro.js
@@ -3,23 +3,31 @@ $(document).ready(function () {
     var startBtn = document.getElementById("startBtn");
 
 
-    var seconds = 59;
-    var minutes = 25;
+    var seconds = 5;
+    var minutes = 1;
     timerDisplay.innerHTML = minutes + ":00";
+    var strtTimer = false;
 
 
 
     startBtn.onclick = function () {
-        minutes -= 1;
-        setInterval(function () {
-            seconds -= 1;
-            if (seconds <= 0) {
-                minutes -= 1;
-                seconds = 59;
-            }
+        if(strtTimer == false) {
+            strtTimer = true;
+            minutes -= 1;
+            setInterval(function () {
+                seconds -= 1;
+                if (seconds <= 0) {
+                    minutes -= 1;
+                    seconds = 59;
+                }
+    
+                timerDisplay.innerHTML = minutes + " : " + seconds;
+            }, 1000);
+        }
+    }
 
-            timerDisplay.innerHTML = minutes + " : " + seconds;
-        }, 1000);
+    if(minutes < 0) {
+        alert("Time is up!");
     }
 
 });
