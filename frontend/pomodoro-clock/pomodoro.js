c249b6874f5eeb9149f2553e4582f50c948cb8a2
Braden Powell <bradenpowell2@gmail.com>
2018-06-25 18:32:38 -0600

    working plus/minus buttons 4 work/break time

--- frontend/pomodoro-clock/pomodoro.js
+++ frontend/pomodoro-clock/pomodoro.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
     var timerDisplay = document.getElementById("timer");
     var startBtn = document.getElementById("startBtn");
+    var workPlus = document.getElementById("workPlus");
+    var workMinus = document.getElementById("workMinus");
+    var breakPlus = document.getElementById("breakPlus");
+    var breakMinus = document.getElementById("breakMinus");
+    var set_workTime = document.getElementById("work");
 
 
-    var seconds = 5;
-    var minutes = 1;
+    var seconds = 59;
+    var minutes = 25;
+    var break_minutes = 5;
     timerDisplay.innerHTML = minutes + ":00";
     var strtTimer = false;
 
-
+    work.innerHTML = minutes + ":00"
+    break_time.innerHTML = break_minutes + ":00";
 
     startBtn.onclick = function () {
         
@@ -18,9 +25,11 @@ $(document).ready(function () {
             minutes -= 1;
             setInterval(function () {
                 if(minutes == 0 && seconds == 1) {
+                    //zero out the clock 
                     minutes = 0; 
                     seconds = 0;
                     timerDisplay.innerHTML = minutes + " : " + seconds;
+                    
                     alert("Time for a Break!");
                     minutes = 5;
                 }
@@ -33,6 +42,32 @@ $(document).ready(function () {
                 timerDisplay.innerHTML = minutes + " : " + seconds;
             }, 1000);
         }
+    }
+
+    workPlus.onclick = function () {
+        minutes += 1; 
+        timerDisplay.innerHTML = minutes + " :00"
+        set_workTime.innerHTML = minutes + " :00"
+    }
+
+    workMinus.onclick = function () {
+        minutes -= 1; 
+        timerDisplay.innerHTML = minutes + " :00"
+        set_workTime.innerHTML = minutes + " :00"
+    }
+    breakPlus.onclick = function () {
+        minutes += 1; 
+        timerDisplay.innerHTML = minutes + " :00"
+    }
+
+    breakPlus.onclick = function () {
+        break_minutes += 1; 
+        break_time.innerHTML = break_minutes + " :00"
+    }
+
+    breakMinus.onclick = function () {
+        break_minutes -= 1; 
+        break_time.innerHTML = break_minutes + " :00"
     }
 
     
